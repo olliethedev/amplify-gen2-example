4477eeb8c62182234fe41f6a3498dca32a283702
olliethedev <3martynov@gmail.com>
2023-11-22 10:50:38 -0500

    format: collection

--- src/layouts/BlogCollection.tsx
+++ src/layouts/BlogCollection.tsx
@@ -1,23 +1,25 @@
-import React from 'react'
-import { fetchBlogs } from '../utils/api'
-import Link from 'next/link';
+import React from "react";
+import { fetchBlogs } from "../utils/api";
+import Link from "next/link";
 
 const BlogCollection = async () => {
   const blogs = await fetchBlogs();
   return (
-    
     <div>
       <h1>Blogs</h1>
       <ul>
-        {blogs && blogs.map((blog) => (
-          <li key={blog.id}>
-            <Link href={`/blog/${blog.id}`}><h2>{blog.title}</h2></Link>
-            <p>{blog.content}</p>
-          </li>
-        ))}
+        {blogs &&
+          blogs.map((blog) => (
+            <li key={blog.id}>
+              <Link href={`/blog/${blog.id}`}>
+                <h2>{blog.title}</h2>
+              </Link>
+              <p>{blog.content}</p>
+            </li>
+          ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
 export default BlogCollection;
